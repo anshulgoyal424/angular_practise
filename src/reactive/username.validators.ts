3aaa0c1388281381b0ebc63fee4d4fb38afa625f
anshul1301 <goyal.anshul@tftus.com>
2019-07-02 13:26:02 +0530

    validation issue resolved

--- src/reactive/username.validators.ts
+++ src/reactive/username.validators.ts
@@ -1,24 +1,23 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class UsernameValidators {
-
-   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
-       console.log(control.value.length);
-        if(( control.value as string).indexOf('') >= 4)
-            return { cannotContainSpace: true};
-
-        return null;
+  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+      console.log(control.value.length);
+      if ((control.value as string).indexOf(' ') >= 0) {
+      return { cannotContainSpace: true };
     }
+      return null;
+  }
 
-    static shouldBeUnique(control : AbstractControl): Promise<ValidationErrors | null >{
-        console.log("hii");
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if(control.value === 'mosh')
-                    resolve({shouldBeUnique: true});
-                else 
-                    resolve(null);
-                }, 2000);
-        });
-    }
+  static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'ansh') {
+          resolve({ shouldBeUnique: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
 }
